92ed3f8c6c74de705b686a473d6878b2ef432aa1
Eric Henke <eahenke@gmail.com>
2020-04-01 17:40:23 -0700

    eslint typography file

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,7 @@
-import Typography from "typography"
-import theme from "typography-theme-noriega"
+import Typography from 'typography';
+import theme from 'typography-theme-noriega';
 
-const typography = new Typography(theme)
-export const { scale, rhythm, options } = typography
-export default typography
+const typography = new Typography(theme);
+
+export const { scale, rhythm, options } = typography;
+export default typography;
