5858d92abd6c5cb80143021cb7e7a7d4197f4e2b
Eric Henke <eahenke@gmail.com>
2020-04-05 15:54:02 -0700

    add social links to header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,53 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import styles from './header.module.css';
+import Container from '../container';
 import Logo from '../logo';
 import Nav from '../nav';
+import SocialLink from '../social-link';
+import { SOCIAL_PLATFORMS } from '../../constants';
 
-const Header = () => (
-    <header className={styles.header}>
-        <div className={styles.logoContainer}>
-            <Logo />
-        </div>
-        <div className={styles.navContainer}>
-            <Nav />
-        </div>
-    </header>
-);
+const Header = () => {
+    const data = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        description
+                        social {
+                            FACEBOOK
+                            INSTAGRAM
+                            TWITTER
+                            ITUNES
+                        }
+                    }
+                }
+            }
+        `
+    );
 
-Header.propTypes = {};
+    const { siteMetadata } = data.site;
+
+    return (
+        <header>
+            <Container>
+                <div className={styles.header}>
+                    <div className={styles.logoContainer}>
+                        <SocialLink platform={SOCIAL_PLATFORMS.FACEBOOK} url={siteMetadata.social.FACEBOOK} />
+                        <SocialLink platform={SOCIAL_PLATFORMS.INSTAGRAM} url={siteMetadata.social.INSTAGRAM} />
+                        <Logo />
+                        <SocialLink platform={SOCIAL_PLATFORMS.TWITTER} url={siteMetadata.social.TWITTER} />
+                        <SocialLink platform={SOCIAL_PLATFORMS.ITUNES} url={siteMetadata.social.ITUNES} />
+                    </div>
+                    <p className={styles.description}>{siteMetadata.description}</p>
+                    <div className={styles.navContainer}>
+                        <Nav />
+                    </div>
+                </div>
+            </Container>
+        </header>
+    );
+};
 
 export default Header;
