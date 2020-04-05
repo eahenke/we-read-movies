0dab1c657c0e33cbd6b9e39b3d246ff98747e94e
Eric Henke <eahenke@gmail.com>
2020-04-05 15:41:55 -0700

    add basic global styles, remove typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,56 @@
+/* eslint-disable import/no-commonjs */
 module.exports = {
-  siteMetadata: {
-    title: `We Read Movies`,
-    description: `The best in bad gay movies`,
-    author: `@gatsbyjs`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: 'We Read Movies',
+        description: 'A podcast about bad gay movies',
+        author: 'Eric Henke',
+        social: {
+            FACEBOOK: 'http://facebook.com',
+            TWITTER: 'http://twitter.com',
+            INSTAGRAM: 'http://instagram.com',
+            ITUNES: 'http://itunes.com'
+        }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `we-read-movies`,
-        short_name: `we-read-movies`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images`
+            }
+        },
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                name: 'we-read-movies',
+                short_name: 'we-read-movies',
+                start_url: '/',
+                background_color: '#663399',
+                theme_color: '#663399',
+                display: 'minimal-ui',
+                icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-prefetch-google-fonts',
+            options: {
+                fonts: [
+                    {
+                        family: 'Montserrat',
+                        variants: ['600']
+                    },
+                    {
+                        family: 'Lora',
+                        variants: ['400']
+                    }
+                ]
+            }
+        }
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // `gatsby-plugin-offline`,
+    ]
+};
