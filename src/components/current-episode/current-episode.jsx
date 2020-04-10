823a2340a81a567c1cc6e3457d50f511b500465e
Eric Henke <eahenke@gmail.com>
2020-04-09 21:46:56 -0700

    redesign current episode component

--- src/components/current-episode/current-episode.jsx
+++ src/components/current-episode/current-episode.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
-import { string, number } from 'prop-types';
+import { string, number, shape } from 'prop-types';
+import { Link } from 'gatsby';
 import Box from '../box';
 import Player from '../player';
 import EpisodeTitle from '../episode-title';
+import ImageWithAttribution from '../image-with-attribution';
 import styles from './current-episode.module.css';
+import { coverArtPropType } from '../../prop-types';
 
-const EpisodePreview = ({ num, title, date, duration, src, description }) => (
+const CurrentEpisode = ({ num, title, date, src, description, cover, slug }) => (
     <Box className={styles.episodeContainer}>
-        {/* Wrap this in a link to dynamic page */}
-        <span className="section-heading">
-            <EpisodeTitle num={num} title={title} />
-        </span>
-        <div className={styles.meta}>
-            <span className={styles.date}>{date}</span>
-            <span className={styles.duration}>Duration: {duration}</span>
-        </div>
-        <div className={styles.playerContainer}>
-            <Player src={src} title={title} />
-        </div>
-        <p className={styles.description}>{description}</p>
+        <Box className={styles.coverContainer}>
+            <ImageWithAttribution src={cover.image.childImageSharp.fluid} text={cover.artist} />
+        </Box>
+        <Box className={styles.episodeInfo} inset="md">
+            <span className="section-heading">
+                <Link to={`episodes/${slug}`}>
+                    <EpisodeTitle num={num} title={title} />
+                </Link>
+            </span>
+            <div className={styles.meta}>
+                <span className={styles.date}>{date}</span>
+            </div>
+            <p className={styles.description}>{description}</p>
+            <div className={styles.playerContainer}>
+                <Player src={src} title={title} />
+            </div>
+        </Box>
     </Box>
 );
-
-EpisodePreview.propTypes = {
+CurrentEpisode.propTypes = {
+    cover: shape(coverArtPropType),
     date: string.isRequired,
     description: string.isRequired,
     duration: string.isRequired,
     num: number.isRequired,
+    slug: string.isRequired,
     src: string,
     title: string.isRequired
 };
 
-export default EpisodePreview;
+export default CurrentEpisode;
