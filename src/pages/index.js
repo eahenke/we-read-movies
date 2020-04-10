b1afdf2263ed6faa1ce781e000b5a893bc491232
Eric Henke <eahenke@gmail.com>
2020-04-09 23:25:18 -0700

    add episode previews to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,54 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { arrayOf, shape } from 'prop-types';
+import { episodePropType } from '../prop-types';
+import { withFlattenedQuery } from '../hocs';
 import PageLayout from '../components/page-layout';
 import Seo from '../components/seo';
 import CurrentEpisode from '../components/current-episode';
-import { episodePropType } from '../prop-types';
-import { withFlattenedQuery } from '../hocs';
+import EpisodePreview from '../components/episode-preview';
+import styles from './index.module.css';
 
 const IndexPage = ({ data }) => {
     const currentEpisode = data.currentEpisodes[0];
+    const { episodes } = data;
 
     return (
         <PageLayout>
             <Seo title="Home" />
             <h1>Current Episode</h1>
             <CurrentEpisode {...currentEpisode} />
+            <div className={styles.episodeList}>
+                {episodes.map(ep => (
+                    <div className={styles.previewWrapper} key={ep.title}>
+                        <EpisodePreview {...ep} />
+                    </div>
+                ))}
+            </div>
         </PageLayout>
     );
 };
 
+// TODO: fix prop types to reflect query
 IndexPage.propTypes = {
     data: shape({
         currentEpisodes: arrayOf(shape(episodePropType))
     })
 };
 
-// TODO: add preview query, fragments
 export const IndexPageQuery = graphql`
     query CurrentEpisode {
         currentEpisodes: allEpisodesJson(sort: { fields: [date], order: DESC }, limit: 1) {
             edges {
                 node {
                     ...episodeFullFragment
+                }
+            }
+        }
+        episodes: allEpisodesJson(sort: { fields: [date], order: DESC }, skip: 1) {
+            edges {
+                node {
+                    ...episodePreviewFragment
                 }
             }
         }
