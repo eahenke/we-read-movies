d2a364fe19bf484ffa4644ab2c7b3a8da5fc3f7e
Eric Henke <eahenke@gmail.com>
2020-04-12 23:49:00 -0700

    correct cover.image prop type to shape

--- src/prop-types/cover-art.js
+++ src/prop-types/cover-art.js
@@ -1,6 +1,7 @@
-import { string } from 'prop-types';
+import { string, shape } from 'prop-types';
 
+// TODO: update prop types to match query results
 export default {
     artist: string,
-    image: string.isRequired
+    image: shape({}).isRequired
 };
