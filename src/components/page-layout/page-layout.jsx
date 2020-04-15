4d084a45cf1d72011a4318947b0154a0d61e6383
Eric Henke <eahenke@gmail.com>
2020-04-14 21:29:46 -0700

    improve site layout spacing

--- src/components/page-layout/page-layout.jsx
+++ src/components/page-layout/page-layout.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import { node } from 'prop-types';
 import Container from '../layout/container';
 import Header from '../header';
+import styles from './page-layout.module.css';
 
 const PageLayout = ({ children }) => (
-    <div className="page-layout">
+    <div className={styles.pageLayout}>
         <Header />
         <main>
             <Container>{children}</Container>
         </main>
-        <footer>
-            <Container>© {new Date().getFullYear()}</Container>
-        </footer>
+        <footer className={styles.footer}>© {new Date().getFullYear()}</footer>
     </div>
 );
 
