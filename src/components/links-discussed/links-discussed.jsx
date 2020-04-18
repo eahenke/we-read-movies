87ec42d370dc2aa80e844266b9035c7cce1e0c91
Eric Henke <eahenke@gmail.com>
2020-04-18 15:33:38 -0700

    fix links overflowing on mobile

--- src/components/links-discussed/links-discussed.jsx
+++ src/components/links-discussed/links-discussed.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { arrayOf, shape, string } from 'prop-types';
+import cx from 'classnames';
 import ExternalLink from '../navigation/external-link';
+import styles from './links-discussed.module.css';
 
 const LinksDiscussed = ({ links = [] }) => {
     if (!links || !links.length) return null;
@@ -8,7 +10,7 @@ const LinksDiscussed = ({ links = [] }) => {
     return (
         <>
             <p className="paragraph-heading">Links Discussed</p>
-            <ul className="body-text">
+            <ul className={cx('body-text', styles.linksDiscussed)}>
                 {links.map((link, idx) => (
                     <li key={idx}>
                         <ExternalLink href={link.url}>{link.text ? link.text : link.url}</ExternalLink>
