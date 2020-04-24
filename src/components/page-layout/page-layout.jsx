bd43686e4e6cfb970dca1ea8dcbf2d87aba22ad5
Eric Henke <eahenke@gmail.com>
2020-04-23 20:06:21 -0700

    remove dev color picker tool

--- src/components/page-layout/page-layout.jsx
+++ src/components/page-layout/page-layout.jsx
@@ -3,16 +3,12 @@ import { node } from 'prop-types';
 import Container from '../layout/container';
 import Header from '../header';
 import styles from './page-layout.module.css';
-import ColorPicker from '../dev-only/color-picker';
 
 const PageLayout = ({ children }) => (
     <div className={styles.pageLayout}>
         <Header />
         <main>
-            <Container>
-                <ColorPicker />
-                {children}
-            </Container>
+            <Container>{children}</Container>
         </main>
         <footer className={styles.footer}>© {new Date().getFullYear()}</footer>
     </div>
